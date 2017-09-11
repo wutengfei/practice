d952c3f95ac9e77008f3ad2a259f909c32d41d90
wutengfei <781862614@qq.com>
2017-09-11 14:34:41 +0800

    新增滴滴笔试题

--- src/com/company/test/Main5.java
+++ src/com/company/test/Main5.java
@@ -1,11 +1,50 @@
 package com.company.test;
 /**
- *
+ *滴滴笔试
+ * 丑数
  */
 
 import java.util.Scanner;
+
 public class Main5 {
+    static int j = 0;
+    static int d[];
+static  int a;
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+         a = in.nextInt();
+        d = new int[a];
+        int j = 0;
+        for (int i = 1; i <= a; i++) {
+          d[j++]= fun(i);
+            if (j==a)
+
+            System.out.println(fun(i));
+        }
+    }
+
+    public static int fun(int n) {
+        if(n<=0) return 0;
+        int[] a=new int[n];
+        a[0]=1;
+        int begin=1,m2=0,m3=0,m5=0;
+        while(begin<n){
+            a[begin]=min(a[m2]*2,a[m3]*3,a[m5]*5);//大于当前最大丑数的最小数
+            while((a[m2]*2<=a[begin])){
+                m2++;
+            }
+            while((a[m3]*3<=a[begin])){
+                m3++;
+            }
+            while((a[m5]*5<=a[begin])){
+                m5++;
+            }
+            begin++;
+        }
+        return a[n-1];
+    }
+    public static int min(int i, int j, int k) {
+        int a=i<j?i:j;
+        return a<k?a:k;
     }
 }
---
+++ src/com/company/test/Main6.java
@@ -0,0 +1,37 @@
+package com.company.test;
+/**
+ *
+ */
+
+import java.util.Scanner;
+
+public class Main6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b[] = new int[a];
+        for (int i = 0; i < a; i++) {
+            b[i] = in.nextInt();
+        }
+        int c = in.nextInt();
+        int[][] d = new int[c][c];
+        for (int i = 0; i < c; i++) {
+            for (int j = 0; j < c; j++) {
+                d[i][j] = in.nextInt();
+            }
+        }
+
+        for (int j = 0; j < c; j++) {
+
+            int e = 0;
+            for (int i = 0; i < a; i++) {
+                if (d[j][0] <=b[i] && b[i] <= d[j][1]) {
+                    if (d[j][2] == b[i])
+                        e++;
+                }
+
+            }
+            System.out.println(e);
+        }
+    }
+}
