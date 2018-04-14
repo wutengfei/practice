7595c43869178b32fa7899b2fa4790dec84fd0f4
wutengfei <781862614@qq.com>
2018-04-14 18:40:26 +0800

    字符串charAt函数

--- src/com/company/others/CharAt.java
+++ src/com/company/others/CharAt.java
@@ -12,10 +12,12 @@ public class CharAt {
         }
 
 
-        String b = " a将";
+        String b = "hello 你好";
         int d = 343454354;
         long f = 12345678903551515L;
-        System.out.println("b=" + b.length());
+        System.out.println("b.length=" + b.length());
+        System.out.println("b.charAt=" + b.charAt(5));
+        System.out.println("b.charAt=" + b.substring(2,7));
         System.out.println(f);
 
     }
