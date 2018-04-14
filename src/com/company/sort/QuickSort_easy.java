c1b452397a5a74d367bb24decad562454f7ad967
wutengfei <781862614@qq.com>
2018-04-14 22:42:34 +0800

    快排添加注释

--- src/com/company/sort/QuickSort_easy.java
+++ src/com/company/sort/QuickSort_easy.java
@@ -3,6 +3,10 @@ package com.company.sort;
 /**
  * 通俗易懂的快排
  * https://blog.csdn.net/vayne_xiao/article/details/53508973
+ * 每次以第一个数为基准数，先从右往左找到第一个比它小的数，再从左往右找到第一个比它大的数，
+ * 然后交换两个数，继续找下去依次交换，直到左右两边相遇为止，这时交换相遇点与基准点的两个数，
+ * 交换之后基准数左边全是比它小的数，右边全是比它大的数。
+ * 然后基准数两边分别递归执行上述操作。
  */
 
 public class QuickSort_easy {
@@ -21,11 +25,11 @@ public class QuickSort_easy {
         i = left;
         j = right;
         while (i < j) {
-            while (a[j] >= temp && i < j)
+            while (a[j] >= temp && i < j)//先从右往左找到比它小的数
                 j--;
-            while (a[i] <= temp && i < j)
+            while (a[i] <= temp && i < j)//再左往右找到比它大的数
                 i++;
-            //交换a[left]、a[right]
+            //交换a[i]、a[j]
             if (i < j) {
                 t = a[i];
                 a[i] = a[j];
